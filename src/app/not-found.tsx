c9449bcdc84e98dad28a4f1bb33b14bb1280a660
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-05-30 21:06:24 +0700

    Add Loading and Not Found components

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="px-4 py-20 text-center">
+      <h2>404 - Links Not Found</h2>
+    </div>
+  );
+}
