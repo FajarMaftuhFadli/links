b5ca67a4390bc6eea7911f0dc74cdbc1fe5a29a8
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-06-12 16:32:04 +0700

    Update Loading component styling to use a simpler background color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="bg-gradient-to-b from-crust to-mantle px-4 pb-20 pt-10 text-center">
+    <div className="bg-crust px-4 pb-20 pt-10 text-center">
       <h2 className="font-semibold">Loading...</h2>
     </div>
   );
