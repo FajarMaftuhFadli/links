3b8cd2166f9ec9fa17f742c79851639a3174d600
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-06-12 16:27:12 +0700

    Add new icons and display engagement metrics in link component

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -1,25 +1,33 @@
 import { Link } from '@/types/linkModel';
 import Image from 'next/image';
 import moment from 'moment';
+import ChatBubbleOvalLeft from '@/icons/chat-bubble-oval-leftt';
+import ArrowPathRoundedSquare from '@/icons/arrow-path-rounded-square';
+import Heart from '@/icons/heart';
+import ChartBar from '@/icons/chart-bar';
 
 export default function Links({ links }: { links: Link[] }) {
+  const randomIntFromInterval = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
   return (
     <section className="flex w-full flex-col bg-crust">
       {links.map((link: Link) => (
         <a
-          className="cursor-default border-b-[1px] border-blue/20 px-4 py-2"
+          className="cursor-default border-b-[1px] border-blue/20 p-4"
           key={link._id}
           href={link.url}
           target="_blank"
           rel="noreferrer noopener"
         >
           {link.pinned === true && (
-            <span className="flex gap-2 pb-2 text-sm font-semibold text-blue/80">
-              <span className="block w-12 text-right">#</span>{' '}
+            <span className="mb-1 flex gap-4 text-sm font-semibold text-blue/80">
+              <span className="block w-12 text-right">#</span>
               <span>Pinned Link</span>
             </span>
           )}
-          <div className="flex gap-2">
+          <div className="mb-2 flex gap-4">
             <div className="h-12 w-12 min-w-[3rem] overflow-hidden rounded-md bg-base">
               <Image src={link.icon} alt={link.name} width={256} height={256} />
             </div>
@@ -40,6 +48,24 @@ export default function Links({ links }: { links: Link[] }) {
                   : link.description}
               </span>
             </div>
+          </div>
+          <div className="flex px-14 text-sm font-light text-blue/80">
+            <span className="flex w-full gap-1 hover:text-blue">
+              <ChatBubbleOvalLeft className="h-5 w-5" />
+              {randomIntFromInterval(5, 99)}
+            </span>
+            <span className="flex w-full gap-1 hover:text-blue">
+              <ArrowPathRoundedSquare className="h-5 w-5" />
+              {randomIntFromInterval(5, 99)}
+            </span>
+            <span className="flex w-full gap-1 hover:text-blue">
+              <Heart className="h-5 w-5" />
+              {randomIntFromInterval(5, 99)}
+            </span>
+            <span className="flex w-full gap-1 hover:text-blue">
+              <ChartBar className="h-5 w-5" />
+              {randomIntFromInterval(5, 10)}.{randomIntFromInterval(1, 99)}k
+            </span>
           </div>
         </a>
       ))}
